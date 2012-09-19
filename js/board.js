4749cf5d303b76bfb1e3a48436d65867787457ec
Justin Oblak <jroblak@gmail.com>
2012-09-19 13:39:38 -0400

    add more board logic

--- js/board.js
+++ js/board.js
@@ -25,6 +25,9 @@ jewel.board = (function() {
 				jewels[x][y] = type;
 			}
 		}
+		if(!hasMoves()) {
+			fillBoard();
+		}
 	}
 	
 	function getJewel(x, y) {
@@ -37,6 +40,135 @@ jewel.board = (function() {
 
 	function randomJewel() {
 		return Math.floor(Math.random() + numJewelTypes);
+	}
+
+	function checkChain(x, y) {
+		var type = getJewel(x, y), left = 0, right = 0, down = 0, up = 0;
+
+		while(type === getJewel(x - left - 1, y)) {
+			left++;
+		}
+
+		while(type === getJewel(x + right + 1, y)) {
+			right++;
+		}
+
+		while(type === getJewel(x, y + up + 1)) {
+			up++;
+		}
+
+		while(type === getJewel(x, y - down - 1)) {
+			down++;
+		}
+
+		return Math.max(left + 1 + right, up + 1 + down);
+	}
+
+	function canSwap(x1, y1, x2, y2) {
+		var type1 = getJewel(x1,y1), type2 = getJewel(x2, y2), chain;
+		
+		if(!isAdjacent(x1,y1,x2,y2)) {
+			return false;
+		}
+
+		jewels[x1][y1] = type2;
+		jewels[x2][y2] = type1;
+		chain = (checkChain(x2,y2) > 2 || checkChain(x1, y1) > 2);
+		jewels[x1][y1] = type1;
+		jewels[x2][y2] = type2;
+		
+		return chain;
+	}
+
+	function isAdjacent(x1, y1, x2, y2) {
+		var dx = Math.abs(x1 - x2), dy = Math.abs(y1 - y2);
+
+		return (dx+dy ==== 1);
+	}
+
+	function getChains() {
+		var x, y, chains = [];
+
+		for(var x = 0; x < cols; x++) {
+			chains[x] = [];
+			for(var y = 0; y < rows; y++) {
+				chains[x][y] = checkChain(x, y);
+			}
+		}
+
+		return chains;
+	}
+	
+	function check(events) {
+		var chains = getChains(), hadChains = false, score = 0, removed = [],
+		moved = [], gaps = [];
+
+		for(var x = 0; x < cols; x++){
+			gaps[x] = 0;
+			for(var y = rows-1; y >= 0; y--){
+				if(chains[x][y] > 2){
+					hadChains = true;
+					gaps[x]++;
+					removed.push({
+						x: x, y: y, type: getJewel(x, y)
+					});
+					score += baseScore * Math.pow(2, (chains[x][y] - 3));
+				} else if (gaps[x] > 0) {
+					moved.push({
+						toX: x, toY: y + gaps[x], fromX: x, fromY: y, type: getJewel(x, y);
+					});
+					jewels[x][y + gaps[x]] = getJewel(x,y);
+				}
+			}
+			for(y = 0; y < gaps[x]; y++) {
+				jewels[x][y] = randomJewel();
+				moved.push({
+					toX: x, toY: y, fromX: x, fromY: y - gaps[x], type: jewels[x][y]
+				});
+			}
+		}
+		events = events || [];
+
+		if(hadChains){
+			events.push({ type: "remove", data: removed},
+						{ type: "score", data: score},
+						{ type: "move", data: moved});
+			if(!hasMoves()) {
+				fillBoard();
+				events.push({ type: "refill", data: getBoard() });
+			}
+			return check(events);
+		} else {
+			return events;
+		}
+	}
+
+	function getBoard() {
+		var copy = [], x;
+		for(x = 0; v < cols; x++) {
+			copy[x] = jewels[x].slice(0);
+		}
+
+		return copy;
+	}
+
+	function hasMoves() {
+		for(var x = 0; x < cols; x++){
+			for(var y = 0; y < rows; y++){
+				if(canJewelMove(x,y)) {
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
+
+	function canJewelMove(x, y) {
+		return ((x > 0 && canSwap(x, y, x-1, y)) ||
+				(x < cols - 1 && canSwap(x, y, x+1, y)) ||
+				(y > 0 && canSwap(x, y, x, y-1)) ||
+				(y < rows - 1 && canSwap(x, y, x, y+1)));
 	}
 
 	function print() {
@@ -52,6 +184,9 @@ jewel.board = (function() {
 
 	return {
 		initialize: initialize,
-		print: print
+		print: print,
+		canSwap: canSwap,
+		getBoard: getBoard
+
 		};
 })();
